14e05bce9d62a6d9cc924b51c020ce3aadf5fd20
filipe-alves-riotinto <filipe.alves.riotinto@gmail.com>
2022-11-09 18:05:57 -0300

    Componente Circulo

--- alganews-cms/src/stories/Confirm.stories.tsx
+++ alganews-cms/src/stories/Confirm.stories.tsx
@@ -4,15 +4,11 @@ import Confirm, { ConfirmProps } from '../components/Confirm/Confirm';
 export default {
   title: 'Example/Confirm',
   component: Confirm,
-  argTypes: {
-    onConfirm: { action: 'confirm' },
-    onCancel: { action: 'cancel' },
-  },
 } as Meta <typeof Confirm>;
 
 const Template: Story<ConfirmProps> = (args) => <Confirm {...args} />;
 
 export const Default = Template.bind({})
 Default.args = {
-  title: 'Você tem certeza?',
+  
 }
