3291ab34097e875392f51f1ede63b356e1344187
filipe-alves-riotinto <filipe.alves.riotinto@gmail.com>
2022-12-13 18:33:32 -0300

    Criação feature UserTopTags

--- alganews-cms/src/app/views/Home.view.tsx
+++ alganews-cms/src/app/views/Home.view.tsx
@@ -1,9 +1,17 @@
 import usePageTitle from "../../core/hooks/usePageTitle"
+import PostListFeature from "../features/PostsList.feature"
+import UserPerformanceFeature from "../features/UserPerformance.feature"
+import UserTopTagsFeature from "../features/UserTopTags.feature"
 import DefaultLayout from "../layouts/Default"
+
+
 
 export default function HomeView() {
     usePageTitle('Home')
+
     return <DefaultLayout>
-        <h1>Home</h1>
+        <UserTopTagsFeature/>
+        <UserPerformanceFeature />
+        <PostListFeature />
     </DefaultLayout>
 }
