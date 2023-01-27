15cdf9c00e77d7dd48e22b845f0e2272c4b66927
filipe-alves-riotinto <filipe.alves.riotinto@gmail.com>
2023-01-27 16:03:12 -0300

    Criação do primeiro serviço 8.14

--- alganews-cms/src/app/features/EditorsList.feature.tsx
+++ alganews-cms/src/app/features/EditorsList.feature.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import styled from "styled-components";
+import PostService from "../../sdk/services/Post.service";
 import CircleChart from "../components/CircleChart";
 import Profile from "../components/Profile";
 
 export default function EditorsListFeature () {
+    useEffect(() => {
+        const posts = PostService.getAllPosts()
+        console.log(posts)
+    }, [])
     return <EditorsListWrapper>
         <Profile editorId={1} name= "Filipe Alves" description="Administrador"/>
         <Profile editorId={2} name= "Eder Riotinto" description="Editor"/>
